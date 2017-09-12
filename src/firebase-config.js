8eaffbe74289e0d2384ffd2081c31b57ab4bf945
hellosirandy <sac840711@gmail.com>
2017-09-12 10:21:38 +0800

    test

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,3 +1,8 @@
 export default {
-  
+  apiKey: "AIzaSyC7DiV2Gttc568Q2LZJaVhC2Gs59xyUj6E",
+  authDomain: "menscave-prod.firebaseapp.com",
+  databaseURL: "https://menscave-prod.firebaseio.com",
+  projectId: "menscave-prod",
+  storageBucket: "menscave-prod.appspot.com",
+  messagingSenderId: "503147034393"
 };
